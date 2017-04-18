bc69fdd911701d69cce85e94ca24c585f8775af9
randyjap <randyjap@gmail.com>
2017-04-18 12:34:05 -0700

    add more spacing, add links to main page, add controller for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'frontpage#index'
 
+  resources :articles, only: [:show]
+
   namespace :admin do
     resources :articles, :users, :sessions
     root to: 'nav#index'
