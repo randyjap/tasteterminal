cd62a06aa13b3286ad0dc67a3c41ccce6e49acb0
randyjap <randyjap@gmail.com>
2017-04-18 12:06:04 -0700

    add custom heading

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,7 +1,23 @@
 class FrontpageController < ApplicationController
   def index
-    category = params[:category]
-    @articles = category ? Article.where(category: category) : Article.all
+    @category = params[:category]
+    @location = params[:location]
+    @user = params[:user]
+    @heading ||= @category
+    @heading ||= @location
+    @heading ||= @user
+    if @category && @location
+      @articles = Article.where(approved: true).where(category: @category).where(location: @location)
+    elsif @category
+      @articles = Article.where(approved: true).where(category: @category)
+    elsif @location
+      @articles = Article.where(approved: true).where(location: @location)
+    elsif @user
+      @articles = Article.where(approved: true).where(user: @user)
+    else
+      @articles = Article.where(approved: true)
+    end
+
     @featured = Article.where(featured: true).limit(4)
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 article = "<div itemprop=\"articleBody\"><p class=\"mol-para-with-font\">The unicorn coffee trend has reached peak saturation.</p><p class=\"mol-para-with-font\">With cafés across the country turning out unicorn-themed lattes, <a style=\"font-weight: bold;\" target=\"_blank\" class=\"\" rel=\"nofollow\" href=\"https://news.starbucks.com/news/starbucks-unicorn-frappuccino\">Starbucks </a>has decided to throw its hat in the ring with its own magical drink, the Unicorn Frappuccino.</p><p class=\"mol-para-with-font\">But like the mystical creature, the drink is quite elusive — and will only be available in stores in the United States, Canada, and Mexico through the end of the weekend.</p><div class=\"artSplitter mol-img-group\">    <div class=\"mol-img\">      <div class=\"image-wrap\">
         <img id=\"i-9c0a5fc1a1c0b572\" src=\"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7\" data-src=\"http://i.dailymail.co.uk/i/pix/2017/04/18/14/3F59DFE300000578-4421640-image-a-2_1492523172385.jpg\" height=\"634\" width=\"634\" alt=\"An a straw for a horn! Starbucks has debuted a new Unicorn Frappuccino\" class=\"blkBorder img-share\"/>
-      </div>"
+      </div><div itemprop=\"articleBody\"><p class=\"mol-para-with-font\">The unicorn coffee trend has reached peak saturation.</p><p class=\"mol-para-with-font\">With cafés across the country turning out unicorn-themed lattes, <a style=\"font-weight: bold;\" target=\"_blank\" class=\"\" rel=\"nofollow\" href=\"https://news.starbucks.com/news/starbucks-unicorn-frappuccino\">Starbucks </a>has decided to throw its hat in the ring with its own magical drink, the Unicorn Frappuccino.</p><p class=\"mol-para-with-font\">But like the mystical creature, the drink is quite elusive — and will only be available in stores in the United States, Canada, and Mexico through the end of the weekend.</p><div class=\"artSplitter mol-img-group\">    <div class=\"mol-img\">      <div class=\"image-wrap\">
+              <img id=\"i-9c0a5fc1a1c0b572\" src=\"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7\" data-src=\"http://i.dailymail.co.uk/i/pix/2017/04/18/14/3F59DFE300000578-4421640-image-a-2_1492523172385.jpg\" height=\"634\" width=\"634\" alt=\"An a straw for a horn! Starbucks has debuted a new Unicorn Frappuccino\" class=\"blkBorder img-share\"/>
+            </div><div itemprop=\"articleBody\"><p class=\"mol-para-with-font\">The unicorn coffee trend has reached peak saturation.</p><p class=\"mol-para-with-font\">With cafés across the country turning out unicorn-themed lattes, <a style=\"font-weight: bold;\" target=\"_blank\" class=\"\" rel=\"nofollow\" href=\"https://news.starbucks.com/news/starbucks-unicorn-frappuccino\">Starbucks </a>has decided to throw its hat in the ring with its own magical drink, the Unicorn Frappuccino.</p><p class=\"mol-para-with-font\">But like the mystical creature, the drink is quite elusive — and will only be available in stores in the United States, Canada, and Mexico through the end of the weekend.</p><div class=\"artSplitter mol-img-group\">    <div class=\"mol-img\">      <div class=\"image-wrap\">
+                    <img id=\"i-9c0a5fc1a1c0b572\" src=\"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7\" data-src=\"http://i.dailymail.co.uk/i/pix/2017/04/18/14/3F59DFE300000578-4421640-image-a-2_1492523172385.jpg\" height=\"634\" width=\"634\" alt=\"An a straw for a horn! Starbucks has debuted a new Unicorn Frappuccino\" class=\"blkBorder img-share\"/>
+                  </div>"
 
-category = ["sim cards", "smart packing", "adventure", "food", "lounge life"]
+category = ["sim cards", "smart packing", "adventure", "food", "lounge life", "best lounges"]
 
 location = ["hong kong", "taiwan", "japan"]
 
@@ -24,26 +28,28 @@ User.create!(
   password: "password"
 )
 
-50.times do
-  Article.create!(
-    author: User.order("RANDOM()").first,
-    title: Faker::Company.catch_phrase,
-    article: article,
-    date: Time.now,
-    category: category.sample,
-    featured: false,
-    approved: true,
-    location: location.sample
-  )
+10.times do
+  6.times do |idx|
+    Article.create!(
+      author: User.order("RANDOM()").first,
+      title: Faker::Company.catch_phrase + " " + category[idx],
+      article: article,
+      date: Time.now,
+      category: category[idx],
+      featured: false,
+      approved: true,
+      location: location.sample
+    )
+  end
 end
 
-10.times do
+5.times do |idx|
   Article.create!(
     author: User.order("RANDOM()").first,
-    title: Faker::Company.catch_phrase,
+    title: Faker::Company.catch_phrase + " " + category[idx],
     article: article,
     date: Time.now,
-    category: category.sample,
+    category: category[idx],
     featured: true,
     approved: true,
     location: location.sample
